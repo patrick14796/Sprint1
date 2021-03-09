69634886eeda29faca87361d119ec07e3930ca3b
patrick14796 <57112628+patrick14796@users.noreply.github.com>
2021-03-09 22:40:39 +0200

    test Work

--- Tests/test1.spec.js
+++ Tests/test1.spec.js
@@ -1,10 +1,21 @@
 var expect = require('chai').expect
 
-describe('Array', () =>{
+describe('Array', () => {
     describe('#sort', () => {
-        it('should sorting array by name', () =>{
-            var months = ['Alice', 'Bob', 'Daniel', 'Vicrtor']
-            expect(months.sort()).to.be.eql(['Alice', 'Bob', 'Daniel', 'Vicrtor'])
+        it('should sorting array by name', () => {
+            var names = ['Daniel', 'Bob', 'vicrtor', 'Alice']
+            expect(names.sort()).to.be.eql(['Alice', 'Bob', 'Daniel', 'vicrtor'])
+        })
+
+        it('sorting number array', () => {
+            expect([5, 2, 1, 6].sort()).to.be.eql([1, 2, 5, 6])
+        })
+    })
+
+    describe('#fillter', () => {
+        it('should filter value', () => {
+            var arr = [1, 2, 3, 4, 5, 6]
+            expect(arr.filter(value => value % 2 == 0)).to.be.eql([2, 4, 6])
         })
     })
 })
